4273b5f6e279b57636c4b1ec7bdd92080c7b9bdd
surathisin <s.surathisin@technosoftautomotive.com>
2024-03-22 14:35:02 +0700

    Updated wsgi.py for PythonAnywhere deployment

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -1,16 +1,14 @@
-"""
-WSGI config for myproject project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+# add your project directory to the sys.path
+project_home = '/home/surathisin/MachineLearningPredictionBoard'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# set environment variable to tell django where your settings.py is
+os.environ['DJANGO_SETTINGS_MODULE'] = 'MachineLearningPredictionBoard.settings'
 
 application = get_wsgi_application()
+
